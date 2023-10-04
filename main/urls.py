8643042d3741d80d422f66aae55049d2a53313fe
KenishaJazlyn <kenishajazlyn@gmail.com>
2023-10-04 15:39:54 +0700

    README TUTORIAL 5 DONE

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from main.views import show_main
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id ,register, login_user, logout_user, edit_product, delete_product
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id ,register, login_user, logout_user, edit_product, delete_product, get_product_json, add_product_ajax
 
 app_name = 'main'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', show_main, name='show_main'), path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'), path('json/', show_json, name='show_json'), path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),path('json/<int:id>/', show_json_by_id, name='show_json_by_id')
     ,path('logout/', logout_user, name='logout'),path('delete/<int:id>', delete_product, name='delete_product'),path('login/', login_user, name='login'), path('edit-product/<int:id>', edit_product, name='edit_product'),path('register/', register, name='register')
-
+    ,path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
